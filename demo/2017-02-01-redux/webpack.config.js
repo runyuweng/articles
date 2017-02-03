584f9f0239d2384afc65d34735052ed10dfe70bb
wengwengweng <593110501@qq.com>
2017-02-03 13:38:44 +0800

    add demo

---
+++ demo/2017-02-01-redux/webpack.config.js
@@ -0,0 +1,33 @@
+
+var config = {
+   entry: './main.js',
+
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+
+   devServer: {
+      inline: true,
+      port: 7777
+   },
+
+   module: {
+      loaders: [{
+         test: /\.jsx?$/,
+         exclude: /node_modules/,
+         loader: 'babel',
+         query: {
+            presets: ['es2015', 'react']
+         }
+      },
+      {
+          test: /\.scss$/,
+          loader: 'style!css!sass'
+      }
+      ]
+   }
+
+}
+
+module.exports = config;
